c26852b74bc055ca1d5f065fc47ff88e9958285d
Kil Hyung-jin <orioncactus@gmail.com>
2023-06-29 19:58:21 +0900

    chore: Update script from axis-replace

--- sources/scripts/kr/axis-replace.py
+++ sources/scripts/kr/axis-replace.py
@@ -1,12 +1,11 @@
 # Update glyphs coordinates and axisRules
 
 import os
-import re
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
-folder_path = os.path.join(script_dir, '../../WantedSans.glyphspackage/glyphs')
+folder_path = os.path.join(script_dir, '../../WantedSans.glyphspackage')
 
-for subdir, dirs, files in os.walk(folder_path):
+for subdir, dirs, files in os.walk(os.path.join(folder_path, 'glyphs')):
     for file in files:
         file_path = os.path.join(subdir, file)
         with open(file_path, 'r') as f:
@@ -15,3 +14,13 @@ for subdir, dirs, files in os.walk(folder_path):
         contents = contents.replace('min = 601;', 'min = 646;')
         with open(file_path, 'w') as f:
             f.write(contents)
+
+for subdir, dirs, files in os.walk(folder_path):
+    for file in files:
+        if file == "fontinfo.plist":
+            file_path = os.path.join(subdir, file)
+            with open(file_path, 'r') as f:
+                contents = f.read()
+            contents = contents.replace('condition 601 < wght;', 'condition 646 < wght;')
+            with open(file_path, 'w') as f:
+                f.write(contents)
