099ed8958317f773d3914bbdca5ef1146185cb36
Kil Hyung-jin <orioncactus@gmail.com>
2023-10-01 22:32:41 +0900

    refactor: Update script

--- scripts/refine/index.sh
+++ scripts/refine/index.sh
@@ -4,6 +4,8 @@ set -e
 script_dir="$(cd "$(dirname "$0")" && pwd)"
 temp_path="$script_dir/temp"
 package_path="$script_dir/../../packages"
+wanted_sans_path="$package_path/wanted-sans/fonts"
+wanted_sans_std_path="$package_path/wanted-sans-std/fonts"
 
 echo "Converting Variable to ttx..."
 for fonts_file in "$temp_path"/*VF.ttf; do
@@ -28,22 +30,24 @@ done
 echo "Moving files to appropriate directories..."
 for fonts_file in "$temp_path"/*; do
     if [[ $fonts_file != *"Std"* ]]; then
-        if [[ $fonts_file == *.otf && $fonts_file != *"Variable"* ]]; then
-            mv "$fonts_file" "$package_path/wanted-sans/fonts/otf"
+        if [[ $fonts_file == *.otf ]]; then
+            target_path="$wanted_sans_path/otf"
         elif [[ $fonts_file == *.ttf && $fonts_file != *"Variable"* ]]; then
-            mv "$fonts_file" "$package_path/wanted-sans/fonts/ttf"
+            target_path="$wanted_sans_path/ttf"
         elif [[ $fonts_file == *.ttf && $fonts_file == *"Variable"* ]]; then
-            mv "$fonts_file" "$package_path/wanted-sans/fonts/variable"
+            target_path="$wanted_sans_path/variable"
         fi
     elif [[ $fonts_file == *"Std"* ]]; then
-        if [[ $fonts_file == *.otf && $fonts_file != *"Variable"* ]]; then
-            mv "$fonts_file" "$package_path/wanted-sans-std/fonts/otf"
+        if [[ $fonts_file == *.otf ]]; then
+            target_path="$wanted_sans_std_path/otf"
         elif [[ $fonts_file == *.ttf && $fonts_file != *"Variable"* ]]; then
-            mv "$fonts_file" "$package_path/wanted-sans-std/fonts/ttf"
+            target_path="$wanted_sans_std_path/ttf"
         elif [[ $fonts_file == *.ttf && $fonts_file == *"Variable"* ]]; then
-            mv "$fonts_file" "$package_path/wanted-sans-std/fonts/variable"
+            target_path="$wanted_sans_std_path/variable"
         fi
     fi
+
+    mv "$fonts_file" "$target_path"
 done
 
 echo "Done!"
